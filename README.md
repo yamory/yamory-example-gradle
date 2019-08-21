b57b28f9032214a0ec8bbdf48e5965c83026f531
Kenichi Suzuki <kenichi.suzuki@bizreach.co.jp>
2019-08-21 16:17:44 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Example Gradle Project
+
+This project is used for demonstration of [yamory](https://yamory.io).
+Please enjoy hunting vulns!
---
+++ subproject-web/src/main/java/io/yamory/example/Main.java
@@ -0,0 +1,10 @@
+package io.yamory.example;
+
+import io.yamory.example.util.ExampleUtil;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println(ExampleUtil.sayHello("yamory"));
+  }
+}
+
